8f5a1ec1ee0f35d799950e1fde1df7548ce737a7
Hydralyph <acidicmc@gmail.com>
2022-05-25 23:44:36 +1200

    Added new moves and updated files.

--- Assets/Scripts/Spirimon.cs
+++ Assets/Scripts/Spirimon.cs
@@ -7,15 +7,37 @@ public class Spirimon
     SpirimonBase sBase;
     int level;
 
+    public int HP { get; set; }
+
+    public List<Move> Moves { get; set; }
+
     Spirimon(SpirimonBase sb, int lvl)
     {
         sBase = sb;
         level = lvl;
+        // HP = sBase.MaxHp; :: UNCOMMENT WHEN MAX UP FORMULA IS CREATED
+
+        Moves = new List<Move>();
+        foreach(var move in sBase.LearnableMoves)
+        {
+            if(move.Level <= level)
+            {
+                Moves.Add(new Move(move.Base));
+            }
+
+            if(Moves.Count >= 4)
+            {
+                break;
+            }
+        }
     }
 
+
+    // Damage formulas
     public int Attack
     {
         get { return Mathf.FloorToInt((2 * sBase.Atk * level / 100) + 5); }
     }
 
+    // ADD MORE HERE
 }
